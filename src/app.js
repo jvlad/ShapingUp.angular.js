b511843bae5ac1cc99bd122ebdb9e03b0be5a1b5
Vlad Zamskoi <v.zamskoi@gmail.com>
2016-01-24 05:58:58 +0300

    level 1 completed.

--- src/app.js
+++ src/app.js
@@ -2,4 +2,15 @@
  * Created by zvlad on 1/24/16.
  */
 
-var app = angular.module('store', []);
+(function () {
+    var app = angular.module('store', []);
+    app.controller('StoreController', function () {
+        this.products = gems;
+    });
+
+    var gems = [
+        {name: 'Azurite', price: 2.95},
+        {name: 'Bloodstone', price: 5.95},
+        {name: 'Zircon', price: 3.95}
+    ];
+})();
