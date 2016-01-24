5d345a6b704737bfca12d898e321f256f877a7b8
Vlad Zamskoi <v.zamskoi@gmail.com>
2016-01-24 16:04:10 +0300

    Tabs added to every product

--- src/app.js
+++ src/app.js
@@ -8,9 +8,54 @@
         this.products = gems;
     });
 
+    app.controller('PanelController', function (){
+        var selectedTabNumber = 1;
+        this.selectTabByNumber = function (tabNumber){
+            selectedTabNumber = tabNumber;
+        };
+
+        this.getSelectedTabNumber = function () {
+            return selectedTabNumber;
+        };
+
+        this.getClassForSpecifiedTab = function (tabNumber) {
+            if (tabNumber === selectedTabNumber){
+                return "active"
+            } else {
+                return "";
+            }
+        }
+    });
+
     var gems = [
-        {name: 'Azurite', price: 2.95},
-        {name: 'Bloodstone', price: 5.95},
-        {name: 'Zircon', price: 3.95}
+        {
+            name: 'Azurite',
+            price: 2,
+            images: [
+                {
+                    full: "../resources/images/dog.jpg"
+                },
+                {
+                    full: "../resources/images/pinguin.jpg"
+                },
+                {
+                    full: "../resources/images/elephant.jpg"
+                }
+            ]
+        },
+        {
+            name: 'Bloodstone',
+            price: 5.95,
+            images: []
+        },
+        {
+            name: 'Zircon',
+            price: 3.95,
+            images: [
+                {
+                    full: "../resources/images/pinguin.jpg"
+                }
+            ]
+        }
     ];
 })();
