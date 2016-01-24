2879fd682d6c35343687084817e6394be89c621e
Vlad Zamskoi <v.zamskoi@gmail.com>
2016-01-24 21:49:41 +0300

    My own implementation of reviewForm

--- src/app.js
+++ src/app.js
@@ -8,9 +8,14 @@
         this.products = gems;
     });
 
-    app.controller('PanelController', function (){
+    app.controller('PanelController', function () {
         var selectedTabNumber = 1;
-        this.selectTabByNumber = function (tabNumber){
+
+        this.selectTabByNumber = function (tabNumber) {
+            if (!tabNumber) {
+                selectedTabNumber = 1;
+                return;
+            }
             selectedTabNumber = tabNumber;
         };
 
@@ -18,8 +23,12 @@
             return selectedTabNumber;
         };
 
+        this.isNumberOfSelectedTab = function (tabNumber) {
+            return (tabNumber === selectedTabNumber);
+        };
+
         this.getClassForSpecifiedTab = function (tabNumber) {
-            if (tabNumber === selectedTabNumber){
+            if (tabNumber === selectedTabNumber) {
                 return "active"
             } else {
                 return "";
@@ -27,10 +36,48 @@
         }
     });
 
+    app.controller('GalleryController', function () {
+        //todo make property inaccessible from outside: implement getter
+        this.current = 0;
+
+        this.setCurrent = function (imageNumber) {
+            if (!imageNumber) {
+                this.current = 0;
+                return;
+            }
+            this.current = imageNumber;
+        };
+    });
+
     var gems = [
         {
             name: 'Azurite',
             price: 2,
+            description: "This is very short undescriptive description of Azurite",
+            specification: "This is very clear and clean specification of Azurite",
+            reviews_deprecated: "These are very positive reviews_deprecated for Azurite",
+            reviews: [
+                //{
+                //    stars: ,
+                //    body: "",
+                //    author: ""
+                //}
+                {
+                    stars: 0,
+                    body: "This full-color book will show you how to make your websites more interactive and your interfaces more interesting and intuitive.",
+                    author: "Jon Duckett"
+                },
+                {
+                    stars: 1,
+                    body: "JavaScript lies at the heart of almost every modern web application, from social apps to the newest browser-based games. Though simple for beginners to pick up and play with, JavaScript is a flexible, complex language that you can use to build full-scale applications.",
+                    author: "Marijn Haverbeke"
+                },
+                {
+                    stars: 2,
+                    body: "It’s easy to learn parts of JavaScript, but much harder to learn it completely—or even sufficiently—whether you’re new to the language or have used it for years. With the 'You Don’t Know JS' book series, you’ll get a more complete understanding of JavaScript, including trickier parts of the language that many experienced JavaScript programmers simply avoid.",
+                    author: "Kyle Simpson"
+                }
+            ],
             images: [
                 {
                     full: "../resources/images/dog.jpg"
@@ -46,11 +93,51 @@
         {
             name: 'Bloodstone',
             price: 5.95,
+            description: "This is very short undescriptive description of Bloodstone",
+            specification: "This is very clear and clean specification of Bloodstone",
+            reviews_deprecated: "These are very positive reviews_deprecated for Bloodstone",
+            reviews: [
+                {
+                    stars: 1,
+                    body: "This full-color book will show you how to make your websites more interactive and your interfaces more interesting and intuitive.",
+                    author: "Jon Duckett"
+                },
+                {
+                    stars: 2,
+                    body: "JavaScript lies at the heart of almost every modern web application, from social apps to the newest browser-based games. Though simple for beginners to pick up and play with, JavaScript is a flexible, complex language that you can use to build full-scale applications.",
+                    author: "Marijn Haverbeke"
+                },
+                {
+                    stars: 3,
+                    body: "It’s easy to learn parts of JavaScript, but much harder to learn it completely—or even sufficiently—whether you’re new to the language or have used it for years. With the 'You Don’t Know JS' book series, you’ll get a more complete understanding of JavaScript, including trickier parts of the language that many experienced JavaScript programmers simply avoid.",
+                    author: "Kyle Simpson"
+                }
+            ],
             images: []
         },
         {
             name: 'Zircon',
             price: 3.95,
+            description: "This is very short undescriptive description of Zircon",
+            specification: "This is very clear and clean specification of Zircon",
+            reviews_deprecated: "These are very positive reviews_deprecated for Zircon",
+            reviews: [
+                {
+                    stars: 2,
+                    body: "This full-color book will show you how to make your websites more interactive and your interfaces more interesting and intuitive.",
+                    author: "Jon Duckett"
+                },
+                {
+                    stars: 3,
+                    body: "JavaScript lies at the heart of almost every modern web application, from social apps to the newest browser-based games. Though simple for beginners to pick up and play with, JavaScript is a flexible, complex language that you can use to build full-scale applications.",
+                    author: "Marijn Haverbeke"
+                },
+                {
+                    stars: 4,
+                    body: "It’s easy to learn parts of JavaScript, but much harder to learn it completely—or even sufficiently—whether you’re new to the language or have used it for years. With the 'You Don’t Know JS' book series, you’ll get a more complete understanding of JavaScript, including trickier parts of the language that many experienced JavaScript programmers simply avoid.",
+                    author: "Kyle Simpson"
+                }
+            ],
             images: [
                 {
                     full: "../resources/images/pinguin.jpg"
