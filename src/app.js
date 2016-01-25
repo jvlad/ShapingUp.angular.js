a867cfe9758db698fb5dec356ac8c898e160b811
Vlad Zamskoi <v.zamskoi@gmail.com>
2016-01-25 05:07:13 +0300

    Product module separated. Cource is finished.

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@
  */
 
 (function () {
-    var app = angular.module('store', []);
+    var app = angular.module('store', ['store-products']);
     app.controller('StoreController', function () {
         this.products = gems;
     });
@@ -31,75 +31,7 @@
         }
     });
 
-    app.directive('productTitle', function () {
-        return {
-            restrict: 'E',
-            templateUrl: 'partials/product-title.html'
-        }
-    });
 
-    app.directive('productImages', function () {
-        return {
-            restrict: 'E',
-            templateUrl: 'partials/product-images.html'
-        }
-    });
-
-    app.directive('productDescription', function () {
-        return {
-            restrict: 'E',
-            templateUrl: 'partials/product-description.html'
-        }
-    });
-
-    app.directive('productSpec', function () {
-        return {
-            restrict: 'E',
-            templateUrl: 'partials/product-spec.html'
-        }
-    });
-
-    app.directive('productReviews', function () {
-        return {
-            restrict: 'E',
-            templateUrl: 'partials/product-reviews.html'
-        }
-    });
-
-    app.directive('productInfoPanels', function () {
-        return {
-            restrict: 'E',
-            templateUrl: 'partials/product-info-panels.html',
-            controller: function () {
-                var selectedTabNumber = 1;
-
-                this.selectTabByNumber = function (tabNumber) {
-                    if (!tabNumber) {
-                        selectedTabNumber = 1;
-                        return;
-                    }
-                    selectedTabNumber = tabNumber;
-                };
-
-                this.getSelectedTabNumber = function () {
-                    return selectedTabNumber;
-                };
-
-                this.isNumberOfSelectedTab = function (tabNumber) {
-                    return (tabNumber === selectedTabNumber);
-                };
-
-                this.getClassForSpecifiedTab = function (tabNumber) {
-                    if (tabNumber === selectedTabNumber) {
-                        return "active"
-                    } else {
-                        return "";
-                    }
-                }
-            },
-            controllerAs: 'panels'
-        }
-    });
 
     var gems = [
         {
---
+++ src/product/product.js
@@ -0,0 +1,78 @@
+/**
+ * Created by zvlad on 1/25/16.
+ */
+
+(function () {
+    var app = angular.module('store-products', []);
+
+    app.directive('productTitle', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'product/partials/product-title.html'
+        };
+    });
+
+    app.directive('productImages', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'product/partials/product-images.html'
+        };
+    });
+
+    app.directive('productDescription', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'product/partials/product-description.html'
+        };
+    });
+
+    app.directive('productSpec', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'product/partials/product-spec.html'
+        };
+    });
+
+    app.directive('productReviews', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'product/partials/product-reviews.html'
+        };
+    });
+
+    app.directive('productInfoPanels', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'product/partials/product-info-panels.html',
+            controller: function () {
+                var selectedTabNumber = 1;
+
+                this.selectTabByNumber = function (tabNumber) {
+                    if (!tabNumber) {
+                        selectedTabNumber = 1;
+                        return;
+                    }
+                    selectedTabNumber = tabNumber;
+                };
+
+                this.getSelectedTabNumber = function () {
+                    return selectedTabNumber;
+                };
+
+                this.isNumberOfSelectedTab = function (tabNumber) {
+                    return (tabNumber === selectedTabNumber);
+                };
+
+                this.getClassForSpecifiedTab = function (tabNumber) {
+                    if (tabNumber === selectedTabNumber) {
+                        return "active"
+                    } else {
+                        return "";
+                    }
+                }
+            },
+            controllerAs: 'panels'
+        };
+    });
+
+})();
